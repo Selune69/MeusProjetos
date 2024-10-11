086eb26aa708514c94d334031bce27860ee5634c
Milene Borges-Lanier <mi_mixlove@hotmail.com>
2024-10-10 21:58:10 -0400

    Atualizacao

--- pythonexercicios/022.py
+++ pythonexercicios/022.py
@@ -7,3 +7,21 @@ estoque = {
 def adicionar_item(item, quantidade):
     if item in estoque:
         estoque[item] += quantidade
+    else:
+        estoque[item] = quantidade
+
+def remover_item(item, quantidade):
+    if item in estoque:
+        estoque[item] -= quantidade
+        if estoque[item] <= 0:
+            del estoque[item]
+    else:
+        print('Item nao encontrado no estoque!')
+
+def consultar_item(item):
+    return estoque.get(item, 'Item nao encontrado no estoque!')
+
+adicionar_item("maca", 5)
+remover_item('banana', 25)
+print(consultar_item('maca'))
+print(estoque)
